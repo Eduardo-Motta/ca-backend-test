164f0cd0c2fd883433418606af581089f3ef2c7e
Elias Lima <eduardomotta.edu@outlook.com>
2024-08-01 02:26:35 -0400

    test: criado testes unitário para o service de criação de cliente (customer)

---
+++ tests/Nexer.Finance.UnitTests/Nexer.Finance.Domain/Services/Customers/CreateCustomerServiceTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Nexer.Finance.Domain.Entities;
+using Nexer.Finance.Domain.Repositories;
+using Nexer.Finance.Domain.Services.Customers;
+using Nexer.Finance.Shared.Utils;
+
+namespace Nexer.Finance.UnitTests.Nexer.Finance.Domain.Services.Customers
+{
+    public class CreateCustomerServiceTest
+    {
+        private readonly Mock<ILogger<CreateCustomerService>> _loggerMock;
+        private readonly Mock<ICustomerRepository> _customerRespositoryMock;
+
+        public CreateCustomerServiceTest()
+        {
+            _loggerMock = new Mock<ILogger<CreateCustomerService>>();
+            _customerRespositoryMock = new Mock<ICustomerRepository>();
+        }
+
+        [Fact]
+        public async void ShouldCreateCustomer()
+        {
+            var entity = new CustomerEntity(name: "João Nascimento", email: "joao.nascimento@email.com", address: "Av. Brasil, 365 - Centro, São Paulo - SP");
+            var cancellationToken = CancellationToken.None;
+
+            _customerRespositoryMock.Setup(repository => repository.CreateCustomerAsync(entity, cancellationToken))
+                .Callback<CustomerEntity, CancellationToken>((cust, token) =>
+                {
+                    typeof(BaseEntity).GetProperty("Id")!.SetValue(cust, 1);
+                })
+                .Returns(Task.CompletedTask);
+
+            var service = new CreateCustomerService(_loggerMock.Object, _customerRespositoryMock.Object);
+
+            var result = await service.CreateCustomerAsync(entity, cancellationToken);
+
+            Assert.True(result.IsRight);
+            Assert.Equal(1, result.Right);
+        }
+
+        [Fact]
+        public async void ShouldReturnErrorHandlingWhenNotCreateCustomer()
+        {
+            var entity = new CustomerEntity(name: "João Nascimento", email: "joao.nascimento@email.com", address: "Av. Brasil, 365 - Centro, São Paulo - SP");
+            var cancellationToken = CancellationToken.None;
+
+            _customerRespositoryMock
+            .Setup(repo => repo.CreateCustomerAsync(It.IsAny<CustomerEntity>(), It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new Exception("Repository error"));
+
+            var service = new CreateCustomerService(_loggerMock.Object, _customerRespositoryMock.Object);
+
+            var result = await service.CreateCustomerAsync(entity, CancellationToken.None);
+
+            Assert.True(result.IsLeft);
+            Assert.IsType<Error>(result.Left);
+            Assert.Equal("An error occurred while creating the customer", result.Left.Message);
+        }
+    }
+}
