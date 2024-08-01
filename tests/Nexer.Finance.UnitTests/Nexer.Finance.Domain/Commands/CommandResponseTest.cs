738525f8f8b2252f872ac52b3ac5718a26b0257e
Elias Lima <eduardomotta.edu@outlook.com>
2024-08-01 02:24:43 -0400

    test: criado testes unitário para os commands de criação de cliente (customer)

---
+++ tests/Nexer.Finance.UnitTests/Nexer.Finance.Domain/Commands/CommandResponseTest.cs
@@ -0,0 +1,87 @@
+﻿using FluentValidation.Results;
+using Nexer.Finance.Domain.Commands;
+
+namespace Nexer.Finance.UnitTests.Nexer.Finance.Domain.Commands
+{
+    internal class CustomerModel
+    {
+        public int Id { get; set; }
+        public string Fullname { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+
+    public class CommandResponseTest
+    {
+        [Fact]
+        public void ShouldReturnMessageError()
+        {
+            string messageErro = "A system failure has occurred";
+            var response = new CommandResponseError(messageErro);
+
+            Assert.False(response.Success);
+            Assert.Equal(messageErro, response.Message);
+        }
+
+        [Fact]
+        public void ShouldReturnErrorListWithPropertiesAndMessage()
+        {
+            IEnumerable<ValidationFailure> failures = new List<ValidationFailure>
+            {
+                new ValidationFailure("Name", "Name is required"),
+                new ValidationFailure("LastName", "LastName must have more than 2 characters"),
+                new ValidationFailure("Cnpj", "Invalid Cnpj"),
+
+            };
+            var response = new CommandResponseErrors(failures);
+
+            Assert.False(response.Success);
+            Assert.Equal(failures.Count(), response.Errors.Count());
+            Assert.Collection(response.Errors,
+                error1 =>
+                {
+                    Assert.Equal("Name", error1.Property);
+                    Assert.Equal("Name is required", error1.Message);
+                    Assert.Null(error1.AttemptedValue);
+                },
+                error2 =>
+                {
+                    Assert.Equal("LastName", error2.Property);
+                    Assert.Equal("LastName must have more than 2 characters", error2.Message);
+                    Assert.Null(error2.AttemptedValue);
+                },
+                error3 =>
+                {
+                    Assert.Equal("Cnpj", error3.Property);
+                    Assert.Equal("Invalid Cnpj", error3.Message);
+                    Assert.Null(error3.AttemptedValue);
+                }
+            );
+        }
+
+        [Fact]
+        public void ShouldReturnIdAsSuccess()
+        {
+            Guid id = Guid.NewGuid();
+            var response = new CommandResponseData<Guid>(id);
+
+            Assert.True(response.Success);
+            Assert.Equal(id, response.Data);
+        }
+
+        [Fact]
+        public void ShouldReturnUserAsSuccess()
+        {
+            CustomerModel customer = new CustomerModel
+            {
+                Id = 10,
+                Fullname = "Linus Torvalds",
+                Email = "contato@email.com",
+            };
+
+            var response = new CommandResponseData<CustomerModel>(customer);
+
+            Assert.True(response.Success);
+            Assert.IsType<CustomerModel>(response.Data);
+        }
+    }
+}
---
+++ tests/Nexer.Finance.UnitTests/Nexer.Finance.Domain/Commands/Customer/Validations/CreateCustomerValidatorTest.cs
@@ -0,0 +1,85 @@
+﻿using Nexer.Finance.Domain.Commands.Customer;
+using Nexer.Finance.Domain.Commands.Customer.Validations;
+
+namespace Nexer.Finance.UnitTests.Nexer.Finance.Domain.Commands.Customer.Validations
+{
+    public class CreateCustomerValidatorTest
+    {
+        [Fact]
+        public async void Should()
+        {
+            var command = new CreateCustomerCommand
+            {
+                Name = "Ana Maria",
+                Email = "anamaria@outlook.com",
+                Address = "Av. Recife, 5502 - Quiosq03 - Centro, Rolim de Moura - RO, 76940-000",
+            };
+            var validate = await new CreateCustomerValidator().ValidateAsync(command);
+
+            Assert.True(validate.IsValid);
+            Assert.Empty(validate.Errors);
+        }
+
+        [Fact]
+        public async void ShouldReturnErrorWhenNameIsEmpty()
+        {
+            var command = new CreateCustomerCommand
+            {
+                Name = string.Empty,
+                Email = "contato@email.com",
+                Address = "Av. Padre Adolpho Rohl, 2478 - Centro, Jaru - RO, 76890-000",
+            };
+            var validate = await new CreateCustomerValidator().ValidateAsync(command);
+
+            Assert.False(validate.IsValid);
+            Assert.Single(validate.Errors);
+            Assert.Collection(validate.Errors, error1 =>
+            {
+                Assert.Equal("Name", error1.PropertyName);
+                Assert.Equal("Required field", error1.ErrorMessage);
+            });
+        }
+
+        [Fact]
+        public async void ShouldReturnErrorWhenNameIsNull()
+        {
+            var command = new CreateCustomerCommand
+            {
+                Name = null!,
+                Email = "carlosjunior@gmail.com",
+                Address = "R. Dr. Gilberto Studart, 55 - Torre Norte - Cocó, Fortaleza - CE, 60192-105",
+            };
+            var validate = await new CreateCustomerValidator().ValidateAsync(command);
+
+            Assert.False(validate.IsValid);
+            Assert.Single(validate.Errors);
+            Assert.Collection(validate.Errors, error1 =>
+            {
+                Assert.Equal("Name", error1.PropertyName);
+                Assert.Equal("Required field", error1.ErrorMessage);
+            });
+        }
+
+        [Fact]
+        public async void ShouldReturnErrorWhenEmailIsInvalid()
+        {
+            var command = new CreateCustomerCommand
+            {
+                Name = "Juca Silva",
+                Email = "jsilva.gmail.com",
+                Address = "Al. Rio Negro, 161 - 4° andar - Alphaville Industrial, Barueri - SP, 06454-000",
+            };
+            var validate = await new CreateCustomerValidator().ValidateAsync(command);
+
+            Assert.False(validate.IsValid);
+            Assert.Single(validate.Errors);
+            Assert.Collection(validate.Errors, error1 =>
+            {
+                Assert.Equal("Email", error1.PropertyName);
+                Assert.Equal("Invalid email address", error1.ErrorMessage);
+            });
+        }
+
+
+    }
+}
