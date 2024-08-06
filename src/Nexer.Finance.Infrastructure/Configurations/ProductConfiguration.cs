5ec92976471418c659f55c2c06de3d3d7c13fac3
Elias Lima <eduardomotta.edu@outlook.com>
2024-08-06 03:50:04 -0400

    feat: implementado classe de configuração da entidade de produto

---
+++ src/Nexer.Finance.Infrastructure/Configurations/ProductConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nexer.Finance.Domain.Entities;
+
+namespace Nexer.Finance.Infrastructure.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<ProductEntity>
+    {
+        public void Configure(EntityTypeBuilder<ProductEntity> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(c => c.Id)
+            .ValueGeneratedOnAdd()
+            .HasColumnType("uuid");
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(p => p.CreatedAt)
+                .IsRequired();
+
+            builder.Property(p => p.UpdatedAt)
+                .IsRequired(false);
+        }
+    }
+}
