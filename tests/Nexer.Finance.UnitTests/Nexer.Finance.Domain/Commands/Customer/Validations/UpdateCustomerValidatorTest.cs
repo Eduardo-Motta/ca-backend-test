8acba4677a9807705642932db0f275a7572a6658
Elias Lima <eduardomotta.edu@outlook.com>
2024-08-06 03:16:37 -0400

    test: criado testes unitário para o service de atualização de clientes (customer)

---
+++ tests/Nexer.Finance.UnitTests/Nexer.Finance.Domain/Commands/Customer/Validations/UpdateCustomerValidatorTest.cs
@@ -0,0 +1,91 @@
+﻿using Nexer.Finance.Domain.Commands.Customer;
+using Nexer.Finance.Domain.Commands.Customer.Validations;
+
+namespace Nexer.Finance.UnitTests.Nexer.Finance.Domain.Commands.Customer.Validations
+{
+    public class UpdateCustomerValidatorTest
+    {
+        [Fact]
+        public async void ShouldValidateWithoutErrors()
+        {
+            var command = new UpdateCustomerCommand
+            {
+                Name = "Ana Maria",
+                Email = "anamaria@outlook.com",
+                Address = "Av. Recife, 5502 - Quiosq03 - Centro, Rolim de Moura - RO, 76940-000",
+            };
+            command.SetId(Guid.NewGuid());
+
+            var validate = await new UpdateCustomerValidator().ValidateAsync(command);
+
+            Assert.True(validate.IsValid);
+            Assert.Empty(validate.Errors);
+        }
+
+        [Fact]
+        public async void ShouldReturnErrorWhenNameIsEmpty()
+        {
+            var command = new UpdateCustomerCommand
+            {
+                Name = string.Empty,
+                Email = "contato@email.com",
+                Address = "Av. Padre Adolpho Rohl, 2478 - Centro, Jaru - RO, 76890-000",
+            };
+            command.SetId(Guid.NewGuid());
+
+            var validate = await new UpdateCustomerValidator().ValidateAsync(command);
+
+            Assert.False(validate.IsValid);
+            Assert.Single(validate.Errors);
+            Assert.Collection(validate.Errors, error1 =>
+            {
+                Assert.Equal("Name", error1.PropertyName);
+                Assert.Equal("Required field", error1.ErrorMessage);
+            });
+        }
+
+        [Fact]
+        public async void ShouldReturnErrorWhenNameIsNull()
+        {
+            var command = new UpdateCustomerCommand
+            {
+                Name = null!,
+                Email = "carlosjunior@gmail.com",
+                Address = "R. Dr. Gilberto Studart, 55 - Torre Norte - Cocó, Fortaleza - CE, 60192-105",
+            };
+            command.SetId(Guid.NewGuid());
+
+            var validate = await new UpdateCustomerValidator().ValidateAsync(command);
+
+            Assert.False(validate.IsValid);
+            Assert.Single(validate.Errors);
+            Assert.Collection(validate.Errors, error1 =>
+            {
+                Assert.Equal("Name", error1.PropertyName);
+                Assert.Equal("Required field", error1.ErrorMessage);
+            });
+        }
+
+        [Fact]
+        public async void ShouldReturnErrorWhenEmailIsInvalid()
+        {
+            var command = new UpdateCustomerCommand
+            {
+                Name = "Juca Silva",
+                Email = "jsilva.gmail.com",
+                Address = "Al. Rio Negro, 161 - 4° andar - Alphaville Industrial, Barueri - SP, 06454-000",
+            };
+            command.SetId(Guid.NewGuid());
+
+            var validate = await new UpdateCustomerValidator().ValidateAsync(command);
+
+            Assert.False(validate.IsValid);
+            Assert.Single(validate.Errors);
+            Assert.Collection(validate.Errors, error1 =>
+            {
+                Assert.Equal("Email", error1.PropertyName);
+                Assert.Equal("Invalid email address", error1.ErrorMessage);
+            });
+        }
+    }
+}
---
+++ tests/Nexer.Finance.UnitTests/Nexer.Finance.Domain/Services/Customers/UpdateCustomerServiceTest.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Nexer.Finance.Domain.Entities;
+using Nexer.Finance.Domain.Repositories;
+using Nexer.Finance.Domain.Services.Customers;
+
+namespace Nexer.Finance.UnitTests.Nexer.Finance.Domain.Services.Customers
+{
+    public class UpdateCustomerServiceTest
+    {
+        private readonly Mock<ILogger<UpdateCustomerService>> _loggerMock;
+        private readonly Mock<ICustomerRepository> _customerRespositoryMock;
+
+        public UpdateCustomerServiceTest()
+        {
+            _loggerMock = new Mock<ILogger<UpdateCustomerService>>();
+            _customerRespositoryMock = new Mock<ICustomerRepository>();
+        }
+
+        [Fact]
+        public async void ShouldUpdateCustomer()
+        {
+            var customerId = Guid.NewGuid();
+            var entity = new CustomerEntity(name: "João Nascimento", email: "joao.nascimento@email.com", address: "Av. Brasil, 365 - Centro, São Paulo - SP");
+            var cancellationToken = CancellationToken.None;
+
+            _customerRespositoryMock.Setup(repository => repository.FindCustomerByIdAsync(customerId, cancellationToken))
+                .ReturnsAsync(entity);
+
+            _customerRespositoryMock.Setup(repository => repository.UpdateCustomerAsync(entity, cancellationToken))
+                .Returns(Task.CompletedTask);
+
+            var service = new UpdateCustomerService(_loggerMock.Object, _customerRespositoryMock.Object);
+
+            var result = await service.UpdateCustomerAsync(customerId, entity, cancellationToken);
+
+            Assert.True(result.IsRight);
+            Assert.IsType<bool>(result.Right);
+        }
+
+        [Fact]
+        public async void ShouldNotUpdateWhenCustomerIsNotFound()
+        {
+            var customerId = Guid.NewGuid();
+            var entity = new CustomerEntity(name: "Adriano Borges", email: "borges.adriano@email.com", address: "Av. Brasil, 365 - Centro, São Paulo - SP");
+            var cancellationToken = CancellationToken.None;
+
+            _customerRespositoryMock.Setup(repository => repository.FindCustomerByIdAsync(customerId, cancellationToken))
+                .ReturnsAsync((CustomerEntity)null!);
+
+            var service = new UpdateCustomerService(_loggerMock.Object, _customerRespositoryMock.Object);
+
+            var result = await service.UpdateCustomerAsync(customerId, entity, cancellationToken);
+
+            Assert.True(result.IsLeft);
+            Assert.Equal("Not found", result.Left.Message);
+        }
+
+        [Fact]
+        public async void ShouldReturnErrorHandlingWhenNotCreateCustomer()
+        {
+            var customerId = Guid.NewGuid();
+            var entity = new CustomerEntity(name: "Aline Junior", email: "al.junior@email.com", address: "Av. Brasil, 365 - Centro, São Paulo - SP");
+            var cancellationToken = CancellationToken.None;
+
+            _customerRespositoryMock.Setup(repository => repository.FindCustomerByIdAsync(customerId, cancellationToken))
+                .ReturnsAsync(entity);
+
+            _customerRespositoryMock.Setup(repository => repository.UpdateCustomerAsync(It.IsAny<CustomerEntity>(), It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new Exception("Repository error"));
+
+            var service = new UpdateCustomerService(_loggerMock.Object, _customerRespositoryMock.Object);
+
+            var result = await service.UpdateCustomerAsync(customerId, entity, cancellationToken);
+
+            Assert.True(result.IsLeft);
+            Assert.Equal("An error occurred while updating the customer", result.Left.Message);
+        }
+    }
+}
