112d3d9b7aac8cdefc0405a5db61b59753138723
Elias Lima <eduardomotta.edu@outlook.com>
2024-08-06 03:18:05 -0400

    test: criado testes unitário para validator de busca de cliente por id (customer)

---
+++ tests/Nexer.Finance.UnitTests/Nexer.Finance.Domain/Commands/Customer/Validations/FindCustomerByIdValidatorTest.cs
@@ -0,0 +1,41 @@
+﻿using Nexer.Finance.Domain.Commands.Customer;
+using Nexer.Finance.Domain.Commands.Customer.Validations;
+
+namespace Nexer.Finance.UnitTests.Nexer.Finance.Domain.Commands.Customer.Validations
+{
+    public class FindCustomerByIdValidatorTest
+    {
+        [Fact]
+        public async void ShouldValidateWithoutErrors()
+        {
+            var command = new FindCustomerByIdCommand
+            {
+                Id = Guid.NewGuid()
+            };
+
+            var validate = await new FindCustomerByIdValidator().ValidateAsync(command);
+
+            Assert.True(validate.IsValid);
+            Assert.Empty(validate.Errors);
+        }
+
+        [Fact]
+        public async void ShouldReturnErrorWhenPageNumberIsEqualsZero()
+        {
+            var command = new FindCustomerByIdCommand
+            {
+                Id = Guid.Empty
+            };
+
+            var validate = await new FindCustomerByIdValidator().ValidateAsync(command);
+
+            Assert.False(validate.IsValid);
+            Assert.Single(validate.Errors);
+            Assert.Collection(validate.Errors, error1 =>
+            {
+                Assert.Equal("Id", error1.PropertyName);
+                Assert.Equal("Required field", error1.ErrorMessage);
+            });
+        }
+    }
+}
