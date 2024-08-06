e8fce27767a6ea58daeae829b2ec6dc3aa357049
Elias Lima <eduardomotta.edu@outlook.com>
2024-08-06 04:15:12 -0400

    feat: configurado injeção de dependência

---
+++ src/Nexer.Finance.Application/Infrastructure/DependencyInjectService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nexer.Finance.Domain.Commands.Customer;
+using Nexer.Finance.Domain.Commands.Products;
+using Nexer.Finance.Domain.Handlers.Customers;
+using Nexer.Finance.Domain.Handlers.Products;
+using Nexer.Finance.Domain.Repositories;
+using Nexer.Finance.Domain.Services.Customers;
+using Nexer.Finance.Domain.Services.Products;
+using Nexer.Finance.Infrastructure.Context;
+using Nexer.Finance.Infrastructure.Repositories;
+using Shared.Handlers;
+
+namespace Nexer.Finance.Application.Infrastructure
+{
+    public static class DependencyInjectService
+    {
+        public static void AddDependencyInject(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DatabaseContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.RegisterRepository();
+            services.RegisterService();
+            services.RegisterHandle();
+        }
+
+        private static void RegisterRepository(this IServiceCollection services)
+        {
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+        }
+
+        private static void RegisterService(this IServiceCollection services)
+        {
+            services.AddScoped<ICreateCustomerService, CreateCustomerService>();
+            services.AddScoped<IUpdateCustomerService, UpdateCustomerService>();
+            services.AddScoped<IFindCustomerService, FindCustomerService>();
+
+            services.AddScoped<ICreateProductService, CreateProductService>();
+            services.AddScoped<IUpdateProductService, UpdateProductService>();
+            services.AddScoped<IFindProductService, FindProductService>();
+        }
+
+        private static void RegisterHandle(this IServiceCollection services)
+        {
+            services.AddScoped<IHandler<FindCustomerByIdCommand>, FindCustomerHandle>();
+            services.AddScoped<IHandler<FindAllCustomersCommand>, FindCustomerHandle>();
+            services.AddScoped<IHandler<CreateCustomerCommand>, CreateCustomerHandle>();
+            services.AddScoped<IHandler<UpdateCustomerCommand>, UpdateCustomerHandle>();
+
+            services.AddScoped<IHandler<FindProductByIdCommand>, FindProductHandle>();
+            services.AddScoped<IHandler<FindAllProductsCommand>, FindProductHandle>();
+            services.AddScoped<IHandler<CreateProductCommand>, CreateProductHandle>();
+            services.AddScoped<IHandler<UpdateProductCommand>, UpdateProductHandle>();
+        }
+    }
+}
--- src/Nexer.Finance.Application/Program.cs
+++ src/Nexer.Finance.Application/Program.cs
@@ -1,9 +1,18 @@
+using Nexer.Finance.Application.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var configuration = builder.Configuration;
+builder.Services.AddDependencyInject(configuration);
 
 var app = builder.Build();
 
