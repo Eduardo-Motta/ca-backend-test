57770333472756a084c0ac258e308ae9fb6fb632
Elias Lima <eduardomotta.edu@outlook.com>
2024-08-06 04:07:43 -0400

    feat: criado repositório de produto

--- src/Nexer.Finance.Domain/Repositories/IProductRepository.cs
+++ src/Nexer.Finance.Domain/Repositories/IProductRepository.cs
@@ -6,7 +6,7 @@ namespace Nexer.Finance.Domain.Repositories
     public interface IProductRepository
     {
         Task CreateProductAsync(ProductEntity product, CancellationToken cancellationToken);
-        Task<ProductEntity> FindProductByIdAsync(Guid productId, CancellationToken cancellationToken);
+        Task<ProductEntity?> FindProductByIdAsync(Guid productId, CancellationToken cancellationToken);
         Task<IEnumerable<ProductEntity>> FindAllProductsAsync(PaginationParameters paginationParameters, CancellationToken cancellationToken);
         Task UpdateProductAsync(ProductEntity product, CancellationToken cancellationToken);
     }
---
+++ src/Nexer.Finance.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nexer.Finance.Domain.Entities;
+using Nexer.Finance.Domain.Repositories;
+using Nexer.Finance.Infrastructure.Context;
+using Nexer.Finance.Shared.Utils;
+
+namespace Nexer.Finance.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly DatabaseContext _context;
+        public ProductRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateProductAsync(ProductEntity product, CancellationToken cancellationToken)
+        {
+            await _context.AddAsync(product, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<IEnumerable<ProductEntity>> FindAllProductsAsync(PaginationParameters paginationParameters, CancellationToken cancellationToken)
+        {
+            int skipCount = (paginationParameters.PageNumber - 1) * paginationParameters.PageSize;
+
+            return await _context.Products
+                .Skip(skipCount)
+                .Take(paginationParameters.PageSize)
+                .ToListAsync(cancellationToken);
+        }
+
+        public async Task<ProductEntity?> FindProductByIdAsync(Guid productId, CancellationToken cancellationToken)
+        {
+            return await _context.Products.Where(x => x.Id == productId).FirstOrDefaultAsync(cancellationToken);
+        }
+
+        public async Task UpdateProductAsync(ProductEntity product, CancellationToken cancellationToken)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
