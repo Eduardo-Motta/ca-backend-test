1ba21ecfdb0a218dd91226e01d788d762fe22f40
Elias Lima <eduardomotta.edu@outlook.com>
2024-08-06 03:25:12 -0400

    feat: criado classes de configurações para entidades e banco de dados

---
+++ src/Nexer.Finance.Infrastructure/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nexer.Finance.Domain.Entities;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Nexer.Finance.Infrastructure.Configurations
+{
+    [ExcludeFromCodeCoverage]
+    public class CustomerConfiguration : IEntityTypeConfiguration<CustomerEntity>
+    {
+        public void Configure(EntityTypeBuilder<CustomerEntity> builder)
+        {
+            builder.ToTable("Customers");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Id)
+            .ValueGeneratedOnAdd()
+            .HasColumnType("uuid");
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(c => c.Email)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(c => c.Address)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(c => c.CreatedAt)
+                .IsRequired();
+
+            builder.Property(c => c.UpdatedAt)
+                .IsRequired(false);
+        }
+    }
+}
---
+++ src/Nexer.Finance.Infrastructure/Context/DatabaseContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nexer.Finance.Domain.Entities;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Nexer.Finance.Infrastructure.Context
+{
+    [ExcludeFromCodeCoverage]
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DatabaseContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<CustomerEntity> Costomers => Set<CustomerEntity>();
+        public DbSet<ProductEntity> Products => Set<ProductEntity>();
+    }
+}
