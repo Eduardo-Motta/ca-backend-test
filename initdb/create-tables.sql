00dd3081a0e461de4ab7de87dc5b87c55722783b
Elias Lima <eduardomotta.edu@outlook.com>
2024-08-07 04:29:36 -0400

    feat: adicionado novas tabelas (Billings, BillingLines) e criado data seeding

--- initdb/create-tables.sql
+++ initdb/create-tables.sql
@@ -13,3 +13,40 @@ CREATE TABLE "Products" (
     "CreatedAt" TIMESTAMP NOT NULL,
     "UpdatedAt" TIMESTAMP
 );
+
+CREATE TABLE "Billings" (
+    "Id" UUID PRIMARY KEY,
+    "CustomerId" UUID NOT NULL,
+    "InvoiceNumber" VARCHAR(50) NOT NULL UNIQUE,
+    "Date" TIMESTAMP NOT NULL,
+    "DueDate" TIMESTAMP NOT NULL,
+    "TotalAmount" DECIMAL(18, 2) NOT NULL,
+    "Currency" VARCHAR(5) NOT NULL,
+    "CreatedAt" TIMESTAMP NOT NULL,
+    "UpdatedAt" TIMESTAMP,
+
+    CONSTRAINT "FK_Customer"
+      FOREIGN KEY("CustomerId")
+	  REFERENCES "Customers"("Id")
+);
+
+CREATE UNIQUE INDEX "IX_InvoiceNumber" ON "Billings" ("InvoiceNumber");
+
+CREATE TABLE "BillingLines" (
+    "Id" UUID PRIMARY KEY,
+    "BillingId" UUID NOT NULL,
+    "ProductId" UUID NOT NULL,
+    "Quantity" DECIMAL(18, 2) NOT NULL,
+    "UnitPrice" DECIMAL(18, 2) NOT NULL,
+    "Subtotal" DECIMAL(18, 2) NOT NULL,
+    "CreatedAt" TIMESTAMP NOT NULL,
+    "UpdatedAt" TIMESTAMP,
+
+    CONSTRAINT "FK_Billings"
+      FOREIGN KEY("BillingId")
+	  REFERENCES "Billings"("Id"),
+
+    CONSTRAINT "FK_Product"
+      FOREIGN KEY("ProductId")
+	  REFERENCES "Products"("Id")
+);
---
+++ initdb/seeding.sql
@@ -0,0 +1,8 @@
+INSERT INTO "Customers" ("Id", "Name", "Email", "Address", "CreatedAt", "UpdatedAt")
+VALUES 
+  ('12081264-5645-407a-ae37-78d5da96fe59', 'Cliente Exemplo 1', 'cliente1@example.com', 'Rua Exemplo 1, 123', '2024-08-07 00:00:00', NULL);
+
+INSERT INTO "Products" ("Id", "Name", "CreatedAt", "UpdatedAt")
+VALUES 
+  ('48c6dc20-a943-4f8f-83ca-1e1cf094a683', 'Produto 1', '2024-08-07 00:00:00', NULL),
+  ('48c6dc20-a943-4f8f-83ca-1e1cf094a612', 'Produto 2', '2024-08-07 00:00:00', NULL);
