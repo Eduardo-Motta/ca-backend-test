d647981a297d88e047821413b382fa333b94b9e1
Elias Lima <eduardomotta.edu@outlook.com>
2024-08-07 04:30:00 -0400

    feat: criado controller para billing

---
+++ src/Nexer.Finance.Application/Controllers/BillingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nexer.Finance.Domain.Services.Billings;
+
+namespace Nexer.Finance.Application.Controllers
+{
+    public class BillingController : BaseController
+    {
+        public BillingController(ILogger<CustomerController> logger) { }
+
+        [HttpPost(("importBillings"))]
+        public async Task<IActionResult> ImportBilling([FromServices] IImportBillingService service, CancellationToken cancellationToken)
+        {
+            var result = await service.ImportaAll(cancellationToken);
+
+            if (result.IsLeft)
+            {
+                return HandleResponse(result.Left);
+            }
+
+            return HandleResponse(result.Right);
+        }
+    }
+}
